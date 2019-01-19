281d06d0f8fc4a1259503f2a58290866b5dc82c6
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-19 20:43:06 +0100

    Create Ejercicio 2.php

---
+++ Ejercicio 2.php
@@ -0,0 +1,14 @@
+<?php
+//Esto es un comentario de una línea
+#Esto también es un comentario de una línea
+
+/*
+Esto es un comentario
+que tiene multiples
+líneas
+*/
+
+// Tu puedes tambien usar comentarios entre el código
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
