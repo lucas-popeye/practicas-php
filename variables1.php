17e565b47493fb822b7c9378332129259e2d6bd0
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-20 16:58:52 +0100

    Create variables1.php

---
+++ variables1.php
@@ -0,0 +1,10 @@
+<?php
+$nombre1 = "Roberto";
+$nombre2 = "Juan";
+echo "$nombre1, $nombre2"; // imprime "Roberto, Juan"
+
+
+$_4site = "aún no"; // válido, comienza con un carácter de subrayado
+$täyte = 'mansikka';    // válido; 'ä' es ASCII (Extendido) 228
+
+?>
