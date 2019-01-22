6ebc29fc61e327fdc0f1546e49dc38051750fbfc
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-22 16:40:24 +0100

    Create $_GET.php

---
+++ $_GET.php
@@ -0,0 +1,15 @@
+<?php
+// recupero el parámetro var desde la URL
+// http://localhost/Ejercicios/index.php?var=35373
+
+echo $_GET['var']; // imprime 35373
+
+// recupero el parámetro var1 y var2 desde la URL
+// http://localhost/Ejercicios/index.php?var1=2&var2=33
+
+echo $_GET['var1']; // imprime 2
+echo $_GET['var2']; // imprime 33
+
+
+
+?>
