9c00d9908f4702485e556b740c50ffcdf4300bb5
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-22 16:40:20 +0100

    Update constantes.php

--- constantes.php
+++ constantes.php
@@ -1,5 +1,8 @@
 <?php
+//primero creamos la constante dandolo un nombre y un valor
 define("name", "value");
 define('Empresa', 'Zend.S.A.');
 define('Autor', 'Luis Miguel Cabezas Granado');
+//para visualizarla en pantalla simplemente escribimos su nombre:
+echo Autor;
 ?>
