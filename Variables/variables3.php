ac827066a427e0b1b946c59cc0b613d0213a57f2
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-22 16:44:26 +0100

    Create variables3.php

---
+++ Variables/variables3.php
@@ -0,0 +1,11 @@
+<?php
+$saludo1 = "Hola, Don Pepito";
+$saludo2 = "Hola, Don José";
+$despedida1  = "Adios, Don Pepito";
+$despedida2  = "Adios, Don José";
+
+echo "$saludo1<br>";
+echo "$saludo2<br>";
+echo "$despedida1<br>";
+echo "$despedida2";
+?>
