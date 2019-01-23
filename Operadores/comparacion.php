bf961a4fc614599c9bc4940f0716ad3125e533ae
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-23 22:18:01 +0100

    Create comparacion.php

---
+++ Operadores/comparacion.php
@@ -0,0 +1,10 @@
+<?php
+$a = 23;
+$b = 75;
+
+if ($a>=$b) { //La condición no se cumple. El resultado es false
+	echo "La comparación es cierta";
+} else{
+	echo "La comparación es falsa porque'$a' es menor que '$b'";
+}
+?>  
