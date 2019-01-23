8357bdb3136e37952718b0e878b446ee07500ec0
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-23 22:16:42 +0100

    Create if.php

---
+++ If/if.php
@@ -0,0 +1,13 @@
+<?php
+$nombre = "Roberto"; //Declaración de la variable $nombre
+
+if ($nombre == "Roberto") //Comprueba la variable $nombre
+{
+	echo "Bienvenido";
+}
+
+/*El valor Roberto se asigna a la variable $nombre y se comprueba
+con ayuda de la instrucción if */
+
+
+?>
