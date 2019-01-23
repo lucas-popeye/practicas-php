409bc109889a8753db22314e5e4dd0858b0a06b0
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-23 22:16:36 +0100

    Create elseif.php

---
+++ If/elseif.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = "Pepe"; //Declaración de la variable $nombre
+
+if ($nombre == "Roberto") //Comprueba la variable $nombre
+{
+	echo "Bienvenido";
+}
+
+else if ($nombre == "Pepe") 
+{
+	echo "Hola";
+}
+
+else 
+{
+	echo "Hasta pronto";
+}
+//Da como resultado "Hola"
+
+?>
