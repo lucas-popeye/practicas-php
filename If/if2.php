51f10c8993ab172304b16eec9b94d6498f0a51a0
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-23 22:17:06 +0100

    Create if2.php

---
+++ If/if2.php
@@ -0,0 +1,19 @@
+<?php
+$edad = 30; //Declaración de la variable $edad
+
+if ($edad > 20) //Comprueba la variable $edad
+{
+	if ($edad == 30) //Compreba la variable $edad
+	 {
+		echo "Bienvenido";
+	}
+	else {
+		echo "Hasta pronto";
+	}
+}
+//Da como resultado "Bienvenido"
+/*El programa comprueba  si la variable $edad es superior a 20, y, como es el 
+caso, el programa comprueba si $edad es igual a 30 y ejecuta echo "Bienvenido" */
+
+
+?>
