c18edd5379d690cdda994eba7ac96aff37a929b8
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-24 12:08:21 +0100

    Create switch4.php

---
+++ Switch/switch4.php
@@ -0,0 +1,18 @@
+<?php
+
+$edad = 25; // declaración de la variable $edad
+
+switch ($edad) // comprueba la variable edad
+{
+ 	case 20:
+ 		echo "Tiene 20 años.";
+ 		break;
+ 	case 25:
+ 		echo "Tiene 25 años.";
+ 		break;
+ 	default:
+ 		echo "No tiene ni 20 ni 25 años.";
+ 		
+}
+
+?>  
