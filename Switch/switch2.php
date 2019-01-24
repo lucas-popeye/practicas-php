3437d955a7ebbee59b97454ad472b3edf08d22f1
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-24 12:08:14 +0100

    Create switch2.php

---
+++ Switch/switch2.php
@@ -0,0 +1,13 @@
+<?php
+
+$nombre = "Roberto"; // declaración de la variable $nombre
+
+switch ($nombre) { //comprueba la variable nombre
+	case 'Roberto':
+		echo "Hola<br>";
+	case 'Juan':
+		echo "Hasta pronto";
+
+}
+
+?>  
