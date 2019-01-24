e583f88c433f68c81385a420b107885e57ea8da7
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-24 12:08:18 +0100

    Create switch3.php

---
+++ Switch/switch3.php
@@ -0,0 +1,18 @@
+<?php
+
+$nombre = "Pepe"; // declaración de la variable $nombre
+
+switch ($nombre) // comprueba la variable nombre
+{
+ 	case 'Roberto':
+ 		echo "Hola";
+ 		break;
+ 	case 'Juan':
+ 		echo "Hasta pronto";
+ 		break;
+ 	default:
+ 		echo "Nadie tiene este nombre";
+ 		
+}
+
+?>  
