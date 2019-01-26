3f11644d94e105c5437f15a7828d15a0e5369ca0
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-26 15:36:53 +0100

    Create concatenacion2.php

---
+++ Operadores/concatenacion2.php
@@ -0,0 +1,8 @@
+<?php
+
+$nombre = "Juan";
+$apellido = "López";
+$nombre = $nombre ." ";//Concatenación de nombre + espacio 
+$nombre = $nombre . $apellido; //Concatenación de nombre + apellido
+echo $nombre
+?>
