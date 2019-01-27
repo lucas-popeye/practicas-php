b94cbfc8b4986855dce5861bed850f79166073a1
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-27 12:06:06 +0100

    Create funcion4.php

---
+++ Funciones/funcion4.php
@@ -0,0 +1,7 @@
+<?php
+function conjunto_palabra($palabra1,$palabra2,$palabra3="Duran") {
+	$devuelve=$palabra1." ".$palabra2." ".$palabra3;
+	return $devuelve;
+}
+echo conjunto_palabra ("Hola,","Juan");
+?>
