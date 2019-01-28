1ba1cad735ac584bb3b43ab88f4978d014a36a62
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-28 12:49:56 +0100

    Delete funcion4.php

--- Funciones/funcion4.php
+++
@@ -1,7 +0,0 @@
-<?php
-function conjunto_palabra($palabra1,$palabra2,$palabra3="Duran") {
-	$devuelve=$palabra1." ".$palabra2." ".$palabra3;
-	return $devuelve;
-}
-echo conjunto_palabra ("Hola,","Juan");
-?>
