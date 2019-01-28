7a9cd8e9fc9a0247ffc8332b2392b059ddfde17b
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-28 12:49:02 +0100

    Delete funcion1.php

--- Funciones/funcion1.php
+++
@@ -1,8 +0,0 @@
-<?php
-function calculo_iva ($precio_bruto) {
-	return $precio_bruto*1.21;
-}
-$precio_iva = calculo_iva (12);
-echo "12 euros sin IVA corresponden a " .$precio_iva. " euros con IVA";
-
-?>
