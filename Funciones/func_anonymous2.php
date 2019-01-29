76a60baf7da9d7bc772407f8f21299818562535b
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-29 12:49:20 +0100

    Create func_anonymous2.php

---
+++ Funciones/func_anonymous2.php
@@ -0,0 +1,9 @@
+<?php
+//declaración de la función anónima
+$func_anonymous = function ($argumento)
+{
+	echo "¡Hola "."$argumento!";
+};
+//ejecución de la función anónima
+$func_anonymous ("Roberto");
+?>
