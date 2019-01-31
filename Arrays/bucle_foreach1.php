8557d3e9f8a25684fbb370af470eb07ac857c298
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-31 21:49:12 +0100

    Create bucle_foreach1.php

---
+++ Arrays/bucle_foreach1.php
@@ -0,0 +1,11 @@
+<?php
+//creación de la tabla:
+$tabla = array("Juan", "Roberto", "Pablo", "Pedro", "Alonso" );
+//bucle en la tabla:
+foreach ($tabla as $val) {
+ 	 
+	//muestra los valores concatenados de la tabla
+	//con un salto de línea
+	echo $val."<br>";
+}
+?>
