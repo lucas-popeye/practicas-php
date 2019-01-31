3a9a0e211872d44e1900c918e83019222dfe824d
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-01-31 21:49:07 +0100

    Create bucle_for.php

---
+++ Arrays/bucle_for.php
@@ -0,0 +1,10 @@
+<?php
+//creación de la tabla:
+$tabla = array("Juan", "Roberto", "Pablo", "Pedro", "Alonso" );
+//bucle en la tabla:
+for ($i=0; $i < 5; $i++) { 
+	//mostrar los valores concatenados de la tabla
+	//con un salto de línea
+	echo $tabla [$i]."<br>";
+}
+?>
