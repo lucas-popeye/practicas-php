ea7094dff84d8fc15c60d9193325b0447035de73
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-01 14:14:01 +0100

    Create funcion_count.php

---
+++ Arrays/funcion_count.php
@@ -0,0 +1,7 @@
+<?php
+//creación de la tabla:
+$tabla = array('A1' =>"Juan", "B4"=>"Roberto", 3=>"Pablo", 
+	"PEPE"=>"Pedro", "H"=>"Alonso" );
+$thisSize = count($tabla);
+echo "El tamaña de la tabla es:".$thisSize;
+?>
