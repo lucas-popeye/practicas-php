e95b74bb737d8c4de6d10641582c162aa36d6595
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-01 14:14:10 +0100

    Create in_array.php

---
+++ Arrays/in_array.php
@@ -0,0 +1,8 @@
+<?php
+//creación de la tabla:
+$tabla = array('A1' =>"Juan", "B4"=>"Roberto", 3=>"Pablo", 
+	"PEPE"=>"Pedro", "H"=>"Alonso" );
+if (in_array("Roberto", $tabla)) {
+	echo "Roberto está en la tabla";
+}
+?>
