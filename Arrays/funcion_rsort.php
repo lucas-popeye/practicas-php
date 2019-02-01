7785f3c21bc86a2f8f26e784eb6f1f79d2e4d37e
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-01 14:33:18 +0100

    Create funcion_rsort.php

---
+++ Arrays/funcion_rsort.php
@@ -0,0 +1,9 @@
+<?php
+//creación de la tabla:
+$tabla = array('A1' =>"Juan", "B4"=>"Roberto", 3=>"Pablo", 
+	"PEPE"=>"Pedro", "H"=>"Alonso" );
+rsort($tabla); //ordena la tabla
+foreach ($tabla as $clave => $valor) {
+	echo "Clave:".$clave.", valor:".$valor. "<br>";
+}
+?>
