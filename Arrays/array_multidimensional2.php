68f85290bbe124e5069b3b557f7f66e4c5f7040c
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-02 12:20:46 +0100

    Create array_multidimensional2.php

---
+++ Arrays/array_multidimensional2.php
@@ -0,0 +1,11 @@
+<?php
+$tab_persona["ESTEFANIA"] ["nombre"] = "PABLO";
+$tab_persona["ESTEFANIA"] ["profesion"] = "ministro";
+$tab_persona["ESTEFANIA"] ["edad"] = 50;
+$tab_persona["LUNA"] ["nombre"] = "ROBERTO";
+$tab_persona["LUNA"] ["profesion"] = "agricultor";
+$tab_persona["LUNA"] ["edad"] = 45;
+
+
+echo $tab_persona ["ESTEFANIA"] ["nombre"];
+?>
