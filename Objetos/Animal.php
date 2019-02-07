e6b2256d61743b52c6a21910eef51a6004b4f147
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-07 21:47:47 +0100

    Create Animal.php

---
+++ Objetos/Animal.php
@@ -0,0 +1,20 @@
+<?php
+	class Animal //palabra clave class seguida del nombre de la clase.
+	{
+		//declaración de atributos:
+		private $color = "gris";
+		private $peso = 10;
+
+		public function comer()
+		{
+			//Método posible para acceder a las propiedades
+			//color y peso
+		}
+
+		public function moverse ()
+		{
+			//Método posible para acceder a las propiedades
+			//color y peso
+		}
+	}
+?>
