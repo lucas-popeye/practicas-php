775672a89ec5bb5fa09f53e8f5a5537ac4a38fe6
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-07 12:48:02 +0100

    Create get_envia.php

---
+++ $_GET/get_envia.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang=«es>
+	<head>
+		<title>Ejercicio con GET</title>
+			<meta http-equiv="Content-Type" content="text/html;
+			 charset=UTF-8">
+	</head>
+	<body>
+		<p><a href="get_recibe.php?apellido=Gómez&nombre=Juan">llama la
+	página get_recibe.php?apellido=Gómez&nombre=Juan</a></p>
+	</body>
+</html>
