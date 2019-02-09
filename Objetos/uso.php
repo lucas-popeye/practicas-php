672453f8967ca32d71d78c1ea992ab71ad63a3f2
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-09 11:58:45 +0100

    Update uso.php

--- Objetos/uso.php
+++ Objetos/uso.php
@@ -4,28 +4,36 @@
 include ('Animal.php');
 
 //instanciar la clase Animal
-$perro = new Animal();
-
-//actualizar el peso del perro
-$perro->setPeso(18);
-//leer el peso
-echo "El peso del perro es: ".$perro->getPeso()."kg<br>";
-//actualizar el color del perro
-$perro->setColor("negro");
-//leer el color
-echo "El color del perro es: ".$perro->getColor()."<br>";
-
-//instanciar la clase Animal
 $gato = new Animal();
 
 //actualizar el peso del gato
-$gato->setPeso(5);
+$gato->setPeso(8);
 //leer el peso
 echo "El peso del gato es: ".$gato->getPeso()."kg<br>";
 //actualizar el color del gato
-$gato>setColor("blanco");
+$gato>setColor("negro");
 //leer el color
 echo "El color del gato es: ".$gato->getColor()."<br>";
 
+//instanciar la clase Animal
+$pez = new Animal();
+
+//actualizar el peso del pez
+$pez->setPeso(1);
+//leer el peso
+echo "El peso del pez es: ".$pez->getPeso()."kg<br>";
+//actualizar el color del pez
+$pez->setColor("blanco");
+//leer el color
+echo "El color del pez es: ".$pez->getColor()."<br>";
+
+//el gato come al pez
+$gato->comer_animal($pez);
+//leer el peso
+echo "El nuevo peso del gato es ".$gato->getPeso ()."kg<br>";
+//leer el peso
+echo "El peso del pez es ".$pez->getPeso ()."kg<br>";
+//leer el color
+echo "El color del pez es: ".$pez->getColor()."<br>";
 
 ?>
