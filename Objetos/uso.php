7a7b3f64b45de4589c094d6c282cc961c84c1a5f
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-14 22:19:41 +0100

    Update uso.php

--- Objetos/uso.php
+++ Objetos/uso.php
@@ -1,39 +1,28 @@
-<?php
+ <?php
 
 //carga de la clase
 include ('Animal.php');
+include ("Pez.class.php");
+include ("Gato.class.php");
 
-//instanciar la clase Animal
-$gato = new Animal();
+//instanciar la clase Pez que llama al constructor de
+//la clase Animal
+$pez = new Pez("gris",8);
+//actualizar el tipo de pez
+$pez->setType(true);
+//instanciar la clase Gato que llama al constructor de la 
+//clase Animal
+$gato = new Gato("blanco",4);
+//llamada al método respira()
+$pez->respira();
+$gato->respira();
 
-//actualizar el peso del gato
-$gato->setPeso(8);
-//leer el peso
-echo "El peso del gato es: ".$gato->getPeso()."kg<br>";
-//actualizar el color del gato
-$gato>setColor("negro");
-//leer el color
-echo "El color del gato es: ".$gato->getColor()."<br>";
 
-//instanciar la clase Animal
-$pez = new Animal();
-
-//actualizar el peso del pez
-$pez->setPeso(1);
-//leer el peso
-echo "El peso del pez es: ".$pez->getPeso()."kg<br>";
-//actualizar el color del pez
-$pez->setColor("blanco");
-//leer el color
-echo "El color del pez es: ".$pez->getColor()."<br>";
-
-//el gato come al pez
-$gato->comer_animal($pez);
-//leer el peso
-echo "El nuevo peso del gato es ".$gato->getPeso ()."kg<br>";
-//leer el peso
-echo "El peso del pez es ".$pez->getPeso ()."kg<br>";
-//leer el color
-echo "El color del pez es: ".$pez->getColor()."<br>";
-
+//llamada al método mostrando los atributos de la clase Pez
+//y animal
+$pez->comer_animal($otro_pez);
+//leer el tipo con el acceso de su propia clase
+echo "El tipo de pez comido es es: ".$otro_pez->getType()."<br>";
+//leer el peso con el acceso de su propia clase
+echo "El peso de pez comido es es: ".$otro_pez->getPeso()."Kg<br>";
 ?>
