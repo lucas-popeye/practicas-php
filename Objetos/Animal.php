7866af47dce1b4baea8a62e540919ce3f25aac99
lucas-popeye <45880102+lucas-popeye@users.noreply.github.com>
2019-02-14 22:19:23 +0100

    Update Animal.php

--- Objetos/Animal.php
+++ Objetos/Animal.php
@@ -1,51 +1,58 @@
 <?php
-	class Animal //palabra clave class seguida del nombre de la clase.
+	abstract class Animal //palabra clave class seguida del nombre de la clase.
 	{
 		//declaración de atributos:
 		private $color = "gris";
 		private $peso = 10;
+		protected $edad = 0;
 
 		//constantes de clase
 		const PESO_LIGERO = 5;
-		const PESO_MEDIO = 10;
+		const PESO_MEDIO= 10;
 		const PESO_PESADO = 15;
 
+		//declaración de la variable estática $contador
+		private static $contador = 0;
 
-		
-		public function _construct ($color,$peso)
-		//constructor que solicita 2 argumentos.
+		public function __construct ($color, $peso)//constructor
+		//que solicita dos argumentos.
 		{
-			echo "Llamada al constructor.<br>";
-			$this->color = $color;//inicialización del color
-			$this->peso = $peso;//inicialización del peso
+				echo "llamada al contructor<br>";
+				$this->color = $color;//Inicialización del color
+				$this->peso = $peso;//Inicialización del peso
+
+			self::$contador = self::$contador + 1;
+
 		}
 
 		//accesos
 		public function getColor()
 		{
-			return $this->color;//devuelve el color
+			return $this->color; //devuelve el color
 		}
+
 		public function setColor()
 		{
 			$this->color = $color; //escrito en el atributo color
 		}
+
 		public function getPeso()
 		{
-			return $this->peso;//devuelve el peso
+			return $this->peso; //devuelve el peso
 		}
+
 		public function setPeso()
 		{
 			$this->color = $peso; //escrito en el atributo peso
 
 		}
 
-
-		//métodos
+		//métodos públicos
 
 		public function comer_animal(Animal $animal_comido)
 		{
-			//El animal que come, aumenta su peso tanto
-			//como el del animal comido
+			//el animal que come aumenta su peso tano como
+			//el del animal comido
 			$this->peso = $this->peso + $animal_comido->peso;
 			//el peso del animal comido y su color se restablecen a 0
 			$animal_comido->peso = 0;
@@ -54,8 +61,7 @@
 
 		public function moverse ()
 		{
-			//Método que pueda acceder a las propiedades
-			//color y peso
+			echo "El animal se mueve.";
 		}
 
 		public function añadir_un_kilo()
@@ -63,5 +69,13 @@
 			$this->peso = $this->peso + 1;
 		}
 		
+		//método estático que devuelve  el valor del contador
+		public static function getContador()
+		{
+			return self::$contador;
+		}
+
+		//código no aplicado por el método abstracto
+		abstract public function respira();
 	}
 ?>
